76a949b6e6eba65d2f7aa8b1e1bd8e10502b5c86
Toem Theara <thearatoem11@gmail.com>
2019-10-07 09:12:39 +0700

    Recycler View

--- app/src/main/java/com/example/phone_shop/MainActivity.kt
+++ app/src/main/java/com/example/phone_shop/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.phone_shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recyclerView)
+        //Set the layout manager
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/phone_shop/Phone.kt
@@ -0,0 +1,10 @@
+package com.example.phone_shop
+
+    data class Phone(
+        var id:Int,
+        var name:String,
+        var price:Int,
+        var brandId:Int,
+        var imageUrl: String
+
+    )
